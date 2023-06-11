fcc9dc76a0cb343a645f48c6795449dfd0383b1f
ltyzzz <ltyzzz2000@gmail.com>
2023-06-11 19:12:25 +0800

    🔧 抽取项目默认roles

--- src/core/commands/gpt/subCommands/chatCommand.ts
+++ src/core/commands/gpt/subCommands/chatCommand.ts
@@ -2,13 +2,7 @@ import { CommandType } from "../../../command";
 import { getGptOutput } from "../gptApi";
 import { defineAsyncComponent } from "vue";
 import ComponentOutputType = GptTerminal.ComponentOutputType;
-
-const roles: string[] = [
-  "default", // 默认角色
-  "cli", // 命令行翻译角色
-  "translator", // 中英文互译角色
-  "sql" // sql翻译角色
-]
+import { roles } from "./roles";
 
 const chatCommand: CommandType = {
   func: "chat",
---
+++ src/core/commands/gpt/subCommands/roles.ts
@@ -0,0 +1,6 @@
+export const roles: string[] = [
+  "default", // 默认角色
+  "cli", // 命令行翻译角色
+  "translator", // 中英文互译角色
+  "sql" // sql翻译角色
+]
